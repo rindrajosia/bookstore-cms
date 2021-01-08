fd29d131b3e05fe067461ddff51c339f434f2daf
rindra josia <rrindrajosia@gmail.com>
2021-01-08 11:07:06 +0300

    install react-redux, redux, prop-types, redux-devtools

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+import './assets/bootstrap.min.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
