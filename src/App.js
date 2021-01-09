1b552faf3873509094a1d132c0d4bce21414446e
rindra josia <rrindrajosia@gmail.com>
2021-01-09 06:09:27 +0300

    add store file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import BooksList from './containers/BooksList';
+import BooksForm from './containers/BooksForm';
 
 const App = () => (
   <div className="container mt-4">
-    <div className="alert alert-success">
-      Bookstore CMS
-    </div>
+    <BooksList />
+    <BooksForm />
   </div>
 );
 
