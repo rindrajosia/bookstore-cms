bcf0dc198b097fd8a015ff0c01ea39b22f39bb43
rindra josia <rrindrajosia@gmail.com>
2021-01-11 13:26:18 +0300

    Implement handleRemoveBook method

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Book = ({ book }) => {
+const Book = ({ book, removeBook }) => {
   const { id, title, category } = book;
   return (
     <tr>
       <td>{id}</td>
       <td>{title}</td>
       <td>{category}</td>
+      <td><button type="button" className="btn btn-danger" onClick={() => removeBook(book)}>Remove Book</button></td>
     </tr>
   );
 };
 
 Book.propTypes = {
   book: PropTypes.oneOfType([PropTypes.object]).isRequired,
+  removeBook: PropTypes.func.isRequired,
 };
 export default Book;
