982f631e83ac5e8336f3575c2d15b30d6efdfebc
rindra josia <rrindrajosia@gmail.com>
2021-01-13 10:44:55 +0300

    Import store inside index file

--- src/index.js
+++ src/index.js
@@ -2,35 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { createStore } from 'redux';
 import App from './components/App';
-import books from './reducers/books';
-
+import store from './redux/store';
 import './assets/bootstrap.min.css';
 
-const initialState = {
-  books: [
-    {
-      id: Math.floor(Math.random() * 10000),
-      title: 'Vision of Evil',
-      category: 'Horror',
-    },
-    {
-      id: Math.floor(Math.random() * 10000),
-      title: 'Considered for Evil',
-      category: 'Horror',
-    },
-    {
-      id: Math.floor(Math.random() * 10000),
-      title: 'Mists of Titan',
-      category: 'Sci-Fi',
-    },
-  ],
-};
-
-const store = createStore(books, initialState,
-  composeWithDevTools());
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
