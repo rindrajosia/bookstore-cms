51c1fc201583373dfae6cd35c9e971d0cc583955
rindra josia <rrindrajosia@gmail.com>
2021-01-13 10:41:41 +0300

    Add filter selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,3 +1,16 @@
+import { CATEGORY_FILTERS } from '../constant/constants';
+
 export const getBooksState = store => store.books;
 
 export const getBookList = store => (getBooksState(store) ? getBooksState(store) : null);
+
+export const getBooksByFilter = (store, filter) => {
+  const allBooks = getBookList(store).books;
+  console.log(allBooks);
+  switch (filter) {
+    case CATEGORY_FILTERS.ALL:
+      return allBooks;
+    default:
+      return allBooks.filter(book => book.category === filter);
+  }
+};
