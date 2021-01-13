de714dbd37362b360b03460acd4809f1dd702e39
rindra josia <rrindrajosia@gmail.com>
2021-01-13 10:46:53 +0300

    Initiliaze book state

--- src/constant/constants.js
+++ src/constant/constants.js
@@ -12,3 +12,23 @@ export const FILTER = [
   'Learning',
   'Sci-Fi',
 ];
+
+export const initialStateBooks = {
+  books: [
+    {
+      id: Math.floor(Math.random() * 10000),
+      title: 'Vision of Evil',
+      category: 'Horror',
+    },
+    {
+      id: Math.floor(Math.random() * 10000),
+      title: 'Considered for Evil',
+      category: 'Horror',
+    },
+    {
+      id: Math.floor(Math.random() * 10000),
+      title: 'Mists of Titan',
+      category: 'Sci-Fi',
+    },
+  ],
+};
--- src/reducers/books.js
+++ src/reducers/books.js
@@ -1,6 +1,7 @@
 import { CREATE_BOOK, REMOVE_BOOK } from '../actions/actionTypes';
+import { initialStateBooks } from '../constant/constants';
 
-const books = (state, action) => {
+const books = (state=initialStateBooks, action) => {
   switch (action.type) {
     case CREATE_BOOK: {
       return {
