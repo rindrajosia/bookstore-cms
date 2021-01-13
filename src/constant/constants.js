fc26d045b98e0bafb71b0ca1431fc3c152ca6af1
rindra josia <rrindrajosia@gmail.com>
2021-01-13 10:39:11 +0300

    Add constant

---
+++ src/constant/constants.js
@@ -0,0 +1,14 @@
+export const CATEGORY_FILTERS = {
+  ALL: 'All',
+};
+
+export const FILTER = [
+  'All',
+  'Action',
+  'Biography',
+  'History',
+  'Horror',
+  'Kids',
+  'Learning',
+  'Sci-Fi',
+];
